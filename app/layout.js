47185c504dcb9e9b8acfeebd77ee401dbc420629
tejasshilare <81355646+tejasshilare@users.noreply.github.com>
2023-09-12 01:33:41 +0530

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+"use client";
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="p-12">{children}</body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,43 @@
+"use client";
+import "./globals.css";
+import React, { useState } from "react";
+import axios from "axios";
+
+export const page = () => {
+  const [Images, setImages] = useState([]);
+  const getImages = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=2&limit=30"
+    );
+    const data = response.data;
+    setImages(data);
+  };
+  return (
+    <div>
+      <>
+        <button
+          type="button"
+          onClick={getImages}
+          className="px-5 py-3 bg-green-800 text-white"
+        >
+          Get Images
+        </button>
+        <div className="p-10">
+          {Images.map((elem, i) => {
+            return (
+              <img
+                key={i}
+                src={elem.download_url}
+                width={300}
+                height={300}
+                className="m-10 rounded inline-block"
+              />
+            );
+          })}
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default page;
