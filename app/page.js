1403df567cd2c71db93ece0927a5d148e4012c63
tejasshilare <81355646+tejasshilare@users.noreply.github.com>
2023-10-31 23:48:45 +0530

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export const page = () => {
         <button
           type="button"
           onClick={getImages}
-          className="px-5 py-3 bg-green-800 text-white"
+          className="px-5 py-3 bg-green-800 text-white transition duration-100 hover:scale-125"
         >
           Get Images
         </button>
@@ -30,7 +30,7 @@ export const page = () => {
                 src={elem.download_url}
                 width={300}
                 height={300}
-                className="m-10 rounded inline-block"
+                className="m-10 rounded inline-block transition duration-100 hover:scale-125"
               />
             );
           })}
